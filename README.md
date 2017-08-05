bf342ceba9e86831ee30205fac7d2398d9ec9bb7
yongyuehuang <1130216245@qq.com>
2017-08-05 14:16:58 +0800

    组件拆分

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## NO
 
-**在这里，我使用echarts提供的模块化加载方式，实现了几个react-echarts图表组件**
+**在这里，我使用echarts提供的模块化加载方式，实现了几个react-echarts图表组件：[react-echarts图表在线渲染查看][1]**
 
 #### 插件版本号
 
@@ -32,8 +32,35 @@
 
 #### 打包成静态文件的大小
 
-js：944kb
+js：944 kb
 
-css：92字节
+css：92 字节
+
+#### github page 开启gzip测试
+
+js：357 kb
+
+css：277 B
 
 
+#### 我们不总是需要插件
+
+实现这些echarts-react组件的目的，是告诉大家，react可以不需要引入第三方插件，使用echarts，不要惧怕react组件！
+
+看过很多人说react难写，因为他们习惯了在jQuery开发模式下导入echarts、swiper、d3等插件。而突然切换到react中，就产生了不知所措的感觉。
+如何在react中导入第三方插件，成为了他们心中的痛点，所以一些人就认为需要别人封装好的echarts-react插件或者其他react插件，才能使用，这种想法是错的。
+
+
+#### echarts体积太大，使用模块化加载
+
+以柱状图为例子，我们根据需要渲染的插件采取模块导入，不渲染的组件不导入，最大程度减小js大小。
+
+```javascript
+import echarts from 'echarts/lib/echarts' //必须
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/grid'
+import 'echarts/lib/chart/bar'
+```
+
+
+[1]: https://hyy1115.github.io/react-echarts-modules/index.html
--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import asyncComponent from './AsyncComponent'
 import { pieOption, barOption, lineOption, scatterOption, mapOption, radarOption, candlestickOption } from './optionConfig/options'
-import PieReact from './EchartsDemo/PieReact' //饼图组件
-import BarReact from './EchartsDemo/BarReact' //柱状图组件
-import LineReact from './EchartsDemo/LineReact' //折线图组件
-import ScatterReact from './EchartsDemo/ScatterReact' //散点图组件
-import MapReact from './EchartsDemo/MapReact' //地图组件
-import RadarReact from './EchartsDemo/RadarReact' //雷达图组件
-import CandlestickReact from './EchartsDemo/CandlestickReact' //k线图组件
+const PieReact = asyncComponent(() => import(/* webpackChunkName: "PieReact" */'./EchartsDemo/PieReact'))  //饼图组件
+const BarReact = asyncComponent(() => import(/* webpackChunkName: "BarReact" */'./EchartsDemo/BarReact')) //柱状图组件
+const LineReact = asyncComponent(() => import(/* webpackChunkName: "LineReact" */'./EchartsDemo/LineReact'))  //折线图组件
+const ScatterReact = asyncComponent(() => import(/* webpackChunkName: "ScatterReact" */'./EchartsDemo/ScatterReact'))  //散点图组件
+const MapReact = asyncComponent(() => import(/* webpackChunkName: "MapReact" */'./EchartsDemo/MapReact'))  //地图组件
+const RadarReact = asyncComponent(() => import(/* webpackChunkName: "RadarReact" */'./EchartsDemo/RadarReact')) //雷达图组件
+const CandlestickReact = asyncComponent(() => import(/* webpackChunkName: "CandlestickReact" */'./EchartsDemo/CandlestickReact')) //k线图组件
 
 class App extends Component {
   render() {
